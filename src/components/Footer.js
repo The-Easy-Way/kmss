7a41074a5ebbb68b40657e37b9d6803b533aba91
Sandesh Shrestha <126408282+Neyywaar@users.noreply.github.com>
2023-03-07 19:32:59 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,7 @@ import {
           <Flex>
             <Avatar size='sm' name='Logo' src={Clear_Logo} />
             <Text paddingLeft={'8px'} fontSize={'20px'} fontWeight={600}>
-              KMSS
+              KMSS Maths Club
             </Text>
           </Flex>
         </a>
